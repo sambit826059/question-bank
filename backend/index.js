b3a9f2eaac3ae4930a9cdf2484a25ad5d80a4079
sambit826059 <sambitmohanty1947@gmail.com>
2024-12-12 19:24:09 +0530

    added prisma and removed node-pg

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,34 @@
 const express = require("express");
+const app = express();
+import createUserSchema from "./db/db";
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+})
+
+app.get("/", (req, res) => {
+  res.send("Server is Healthy");
+})
+
+app.post("/create-user", (req, res) => {
+  const { first_name, middle_name, last_name, email, password } = req.body;
+
+  if (first_name || !middle_name || !last_name || !email || !password) {
+    return res.status(400).json({ error: "MIssing required values" });
+  }
+
+  const query =
+    INSERT INTO users (first_name, middle_name, last_name, email, password)
+  VALUES($1, $2, $3, $4, $5)
+  RETURNING *;
+  `;
+
+})
 
 
+app.listen(5000);

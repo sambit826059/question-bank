19e40284282fd9f2564d5a0f3251abeeaf355195
sambit826059 <sambitmohanty1947@gmail.com>
2024-12-18 06:45:25 +0530

    added post router syntax

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userRouter = require("./user");
+const postRouter = require("./post");
 
 const router = express.Router();
 
 router.use("/user", userRouter)
+router.use("/post", postRouter)
 
 module.exports = router;

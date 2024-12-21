f6f64d6ef64147e6dcf24b30c063c71572e6134b
sambit826059 <sambitmohanty1947@gmail.com>
2024-12-21 20:59:50 +0530

    route to landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Page from "./components/Page";
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Page />} />
+          {/* <Route path="/signup" element={<Signup />} /> */}
+          {/* <Route path="/signin" element={<Signin />} /> */}
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
